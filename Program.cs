20922f32755d83be876f43a96c7198f5e7c60baf
Lakshmy <lovely_lakshmy@mail.ru>
2023-02-02 11:15:00 +0300

    v 1.0.0. Добавленн ввод исходного массива

--- Program.cs
+++ Program.cs
@@ -6,10 +6,35 @@ using System.Threading.Tasks;
 
 namespace FinalTestWork
 {
+
     class Program
     {
+        //Ввод исходного массива
+        static string[] GetArrayInput(int n)
+        {
+            string[] array = new string[n];
+
+            Console.WriteLine("Введите строки массива:");
+            for (int i = 0; i < n; i++)
+                array[i] = Console.ReadLine();
+
+            Console.WriteLine();
+            return array;
+        }
+
         static void Main(string[] args)
         {
+            Console.Write("Введите количество строк в массиве: ");
+            int countInputArray = 0;
+            int.TryParse(Console.ReadLine(), out countInputArray);
+            if (countInputArray <= 0)
+            {
+                Console.WriteLine("Количество элементов должно быть больше или равно 1!");
+                return;
+            }
+            Console.WriteLine();
+
+            string[] arrayInput = GetArrayInput(countInputArray);
         }
     }
 }
